9972ea2a3c6163cae9837736e4d535365888fe84
Prabin Subedi <42871240+prabincankod@users.noreply.github.com>
2025-10-19 19:25:24 +0545

    prettier.

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,52 +1,43 @@
 // 1. Import utilities from `astro:content`
-import { defineCollection, z } from 'astro:content';
+import { defineCollection, z } from "astro:content";
 
 // 2. Import loader(s)
-import { glob, file } from 'astro/loaders';
+import { glob, file } from "astro/loaders";
 
 const sloks = defineCollection({
-
-    loader: glob({
-        pattern: '**/*/index.json', base: './src/content/slok',
+  loader: glob({
+    pattern: "**/*/index.json",
+    base: "./src/content/slok",
+  }),
+  schema: z.object({
+    chapter: z.number(),
+    slok: z.union([z.string(), z.number()]),
+    verse: z.number(),
+    transliteration: z.string(),
+    raman: z.object({
+      sc: z.string(),
+      et: z.string(),
     }),
-    schema: z.object({
-        "chapter": z.number(),
-        "slok": z.union([z.string(), z.number()]),
-        "verse": z.number(),
-        'transliteration': z.string(),
-        'raman': z.object({
-            'sc': z.string(),
-            'et': z.string()
-        })
-    })
+  }),
 });
-
 
 const chapters = defineCollection({
-
-    loader: glob({ pattern: '**/index.json', base: './src/content/chapter', }),
-    schema: z.object({
-        "chapter_number": z.number(),
-        "verses_count": z.number(),
-        "name": z.string(),
-        'transliteration': z.string(),
-        'meaning': z.object({
-            'en': z.string(),
-            "hi": z.string()
-        })
-
-        ,
-
-        'summary': z.object({
-
-            en: z.string(),
-            hi: z.string(),
-        })
-    })
+  loader: glob({ pattern: "**/index.json", base: "./src/content/chapter" }),
+  schema: z.object({
+    chapter_number: z.number(),
+    verses_count: z.number(),
+    name: z.string(),
+    transliteration: z.string(),
+    meaning: z.object({
+      en: z.string(),
+      hi: z.string(),
+    }),
+    summary: z.object({
+      en: z.string(),
+      hi: z.string(),
+    }),
+  }),
 });
-
-
-
 
 // 4. Export a single `collections` object to register your collection(s)
 export const collections = { sloks, chapters };
