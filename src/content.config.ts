2c8098bf923511a00ac228fd394e6cc25d7c98d6
Prabin Subedi <42871240+prabincankod@users.noreply.github.com>
2025-11-01 00:16:46 +0545

    old.

--- src/content.config.ts
+++ src/content.config.ts
@@ -6,42 +6,57 @@ import { glob, file } from "astro/loaders";
 
 const sloks = defineCollection({
   loader: glob({
-    pattern: "**/*/index.json",
-    base: "./src/content/slok",
+    pattern: "**/*.json",
+    base: "./src/content/sloks",
   }),
   schema: z.object({
-    chapter: z.number(),
-    slok: z.union([z.string(), z.number()]),
-    verse: z.number(),
+    chapter_id: z.number(),
+    chapter_number: z.number(),
+    externalId: z.number(),
+    id: z.number(),
+    text: z.string(),
+    title: z.string(),
+    verse_number: z.number(),
+    verse_order: z.number(),
     transliteration: z.string(),
-    raman: z.object({
-      sc: z.string(),
-      et: z.string(),
-    }),
-    prabhu: z.object({
-      ec: z.string(),
-      et: z.string(),
-    }).optional(),
-  }),
+    word_meanings: z.string()
+  })
+
 });
 
 const chapters = defineCollection({
-  loader: glob({ pattern: "**/index.json", base: "./src/content/chapter" }),
+  loader: glob({ pattern: "**/index.json", base: "./src/content/chapters" }),
   schema: z.object({
     chapter_number: z.number(),
-    verses_count: z.number(),
+    chapter_summary: z.string(),
+    chapter_summary_hindi: z.string(),
+    id: z.number(),
+    image_name: z.string(),
     name: z.string(),
-    transliteration: z.string(),
-    meaning: z.object({
-      en: z.string(),
-      hi: z.string(),
-    }),
-    summary: z.object({
-      en: z.string(),
-      hi: z.string(),
-    }),
-  }),
+    name_meaning: z.string(),
+    name_translation: z.string(),
+    name_transliterated: z.string(),
+    verses_count: z.number()
+  })
 });
 
+const translations = defineCollection({
+  loader: glob({ pattern: "**/*.json", base: "./src/content/translations", }),
+  schema: z.array(
+    z.object({
+      authorName: z.string(),
+      author_id: z.number(),
+      description: z.string(),
+      id: z.number(),
+      lang: z.string(),
+      language_id: z.number(),
+      verseNumber: z.number(),
+      verse_id: z.number()
+    })
+  )
+
+
+})
+
 // 4. Export a single `collections` object to register your collection(s)
-export const collections = { sloks, chapters };
+export const collections = { sloks, chapters, translations };
